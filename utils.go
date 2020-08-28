f2321ef519f05dcdefb65cb3af326c802c6e5448
Dan Sikes <dansikes7@gmail.com>
2020-08-28 02:19:27 -0400

    working version around bbolt

--- utils.go
+++ utils.go
@@ -6,9 +6,13 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
 )
 
 func createHash(key string) string {
@@ -17,46 +21,59 @@ func createHash(key string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func encrypt(data []byte, passphrase string) []byte {
+func encrypt(data []byte, passphrase string) ([]byte, error) {
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
+	return ciphertext, nil
 }
 
-func decrypt(data []byte, passphrase string) []byte {
+func decrypt(data []byte, passphrase string) ([]byte, error) {
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
-	return plaintext
+	return plaintext, nil
 }
 
-func encryptFile(filename string, data []byte, passphrase string) {
+func encryptFile(filename string, data []byte, passphrase string) error {
 	f, _ := os.Create(filename)
 	defer f.Close()
-	f.Write(encrypt(data, passphrase))
+	enc, err := encrypt(data, passphrase)
+	if err != nil {
+		return err
+	}
+	f.Write(enc)
+	return nil
 }
 
-func decryptFile(filename string, passphrase string) []byte {
+func decryptFile(filename string, passphrase string) ([]byte, error) {
 	data, _ := ioutil.ReadFile(filename)
 	return decrypt(data, passphrase)
+}
+
+// generate a OTP key for a given namespace
+func generateOTPKey(namespace string) (*otp.Key, error) {
+	return totp.Generate(totp.GenerateOpts{
+		Issuer:      "lockbox",
+		AccountName: fmt.Sprintf("%v@lockbox", namespace),
+	})
 }
